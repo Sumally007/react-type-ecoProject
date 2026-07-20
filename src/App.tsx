79505e3be3d767f49b673c7ab144509c973f1ee6
Sumally007 <ismailmbarouk007@gmail.com>
2026-07-20 17:16:40 +0300

    creating filterContext

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
+import Sidebar from "./components/Sidebar";
+
 function App() {
   return (
-    <div className="min-h-screen bg-slate-900 flex items-center justify-center">
-      <h1 className="text-4xl font-bold text-white">Habari Ismail! 👋</h1>
+    <div>
+      <Sidebar />
     </div>
   )
 }
