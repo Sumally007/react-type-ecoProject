e896eb65866e602f7a91a85ced4fd2692f77af96
Ismail Said <ismailmbarouk007@gmail.com>
2026-07-31 14:19:15 +0300

    productPage to w-full in mobile

--- src/components/ProductPage.tsx
+++ src/components/ProductPage.tsx
@@ -32,7 +32,7 @@ const ProductPage = () => {
         return <h1>Loading...</h1>
     }
     return (
-        <div className="p-5 w-[60%]">
+        <div className="p-5 w-full sm:w-[60%]">
             <button
                 onClick={() => navigate(-1)}
                 className="mb-5 px-4 py-2 bg-black text-white rounded cursor-pointer">
