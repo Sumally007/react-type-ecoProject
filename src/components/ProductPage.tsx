79c522dee7342b37464322cd1cebc6d9d69978f9
Ismail Said <ismailmbarouk007@gmail.com>
2026-09-10 20:53:14 +0300

    commenting out themeToggle at productPage

--- src/components/ProductPage.tsx
+++ src/components/ProductPage.tsx
@@ -59,7 +59,7 @@ const ProductPage = () => {
                 Back
             </button>
 
-            <ThemeToggle />
+            {/* <ThemeToggle /> */}
 
             {/* LOADING STATE:*/}
             {isLoading && (
